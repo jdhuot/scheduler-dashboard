64c1be7671909a0b2870a498abe7e7bd0e4b3ceb
Jeremy Huot <logflume@live.com>
2020-08-13 15:54:53 -0600

    Add functionality so when user clicks on a panel, the focused state updates with that panel id, then when they click again, focused is set back to null. Also add set and check the localStorage with focused state to persist state across browser refreshes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,15 +29,63 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   }
 
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
+
+  selectPanel(id) {
+    if (this.state.focused === id) {
+      this.setState({
+        focused: null
+      });
+    } else {
+      this.setState({
+        focused: id
+       });
+    }
+   }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+
+    const panels = data
+      .filter((panel) => {
+        return this.state.focused === null || this.state.focused === panel.id
+      })
+      .map((panel) => {
+        return(<Panel 
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+        />);
+    })
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
     if (this.state.loading) {
       return <Loading />;
     } else {
-      return <main className={dashboardClasses} />;
+      return (
+        <main className={dashboardClasses}> 
+          {panels}
+        </main>
+      );
     }
   }
 }
